2f5a044352455365c104f3cdbd9bbfc3f3fc0e85
jousemarquez <mquezflores@gmail.com>
2023-02-16 12:48:13 +0100

    Actualización JuegoDeRol

--- src/JuegoDeRol/Main.java
+++ src/JuegoDeRol/Main.java
@@ -3,6 +3,12 @@ package JuegoDeRol;
 public class Main {
     public static void main(String[] args) {
 
+/*
+        PersonajePrincipal Timmy = new PersonajePrincipal();
+
+        Timmy.getClass();
+        System.out.println(Timmy);
+*/
         PersonajePrincipal.saludMaxima = 100;
         PersonajePrincipal.saludActual = 70;
         PersonajePrincipal.fuerza = 5;
@@ -15,37 +21,16 @@ public class Main {
 
         System.out.println("Estoy muerto?: " + PersonajePrincipal.estaMuerto());
         System.out.println("Has cogido un botiquin...");
-        Acciones.curar(5);
+        PersonajePrincipal.curar(0);
         System.out.println("Salud actual: " + PersonajePrincipal.saludActual);
         System.out.println("Te atacan por la espalda...");
-        Acciones.recibirDaño(20);
+        PersonajePrincipal.recibirDaño(20);
         if (PersonajePrincipal.estaMuerto()) {
             System.out.println("GAME OVER");
         } else {
             System.out.println("Has cogido un botiquin...");
-            Acciones.curar(25);
+            PersonajePrincipal.curar(25);
             System.out.println("La vida del personaje es " + PersonajePrincipal.saludActual);
-        }
-    }
-
-    public class PersonajePrincipal {
-
-        public static String nombre;
-        public static int edad;
-        public static int altura;
-        public static int saludMaxima;
-        public static int saludActual;
-        // atributos SPECIAL
-        public static int fuerza;
-        public static int percepcion;
-        public static int resistencia;
-        public static int agilidad;
-        public static int inteligencia;
-        public static int carisma;
-        public static int suerte;
-
-        public static boolean estaMuerto() {
-            return saludActual <= 0;
         }
     }
 }
