e67c69df24aaa6a9a3ff8efcfe1cafe4f77dc1e6
jousemarquez <mquezflores@gmail.com>
2023-02-22 10:49:59 +0100

    Comienzo ejercicio CCC

--- src/POO/CuentasBancarias/CCC.java
+++ src/POO/CuentasBancarias/CCC.java
@@ -1,2 +1,91 @@
-package POO.CuentasBancarias;public class CCC {
+package POO.CuentasBancarias;
+
+public class CCC {
+
+    private double saldoDeCuenta;
+    private String nombreDeTitular;
+    private long numeroDeCuenta;
+
+
+    public CCC(){
+        nombreDeTitular = "DefaultName";
+    }
+
+    public Television(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public Television(String marca, String modelo, int volumen, int brillo, int contraste, int canal) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.volumen = volumen;
+        this.brillo = brillo;
+        this.contraste = contraste;
+        this.canal = canal;
+    }
+
+    // GETTERS Y SETTERS
+    // No recibe un valor sino devuelve un argumento
+    public String getMarca(){
+        return marca;
+    }
+    // Siempre es void y no retorna nada sino que recibe un argumento.
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+    // Siempre es void y no retorna nada sino que recibe un argumento.
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public int getVolumen(){
+        return volumen;
+    }
+    // Siempre es void y no retorna nada sino que recibe un argumento.
+    public void setVolumen(int volumen){
+        this.volumen = volumen;
+    }
+
+    public int getBrillo() {
+        return brillo;
+    }
+
+    public void setBrillo(int brillo) {
+        this.brillo = brillo;
+    }
+
+    public int getContraste() {
+        return contraste;
+    }
+
+    public void setContraste(int contraste) {
+        this.contraste = contraste;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    // Los métodos se ponen SIEMPRE debajo de todos los constructores por convención.
+    /*public String toString(){
+        // Genera una cadena que tenga representación de este métod y devolverlo.
+        String cad = "Television: " +marca+ "/" +modelo+ "/" +volumen+ "/" +brillo+ "/" +contraste+ "/" +canal;
+        return cad;
+    }*/
+
+    // This
+    // Es la referencia sobre el objeto que se está invocando el método.
+    public void imprimirReferencia(){
+        System.out.print("La referenia de este objeto es: ");
+        System.out.println(this);
+    }
 }
--- src/POO/CuentasBancarias/Main.java
+++ src/POO/CuentasBancarias/Main.java
@@ -1,2 +1,59 @@
-package POO.CuentasBancarias;public class Main {
+package POO.CuentasBancarias;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Contructor de objetos
+        Television tele1 = new Television();
+        System.out.println(tele1);
+        System.out.println(tele1.getCanal());
+        System.out.println(tele1.getMarca());
+        tele1.setMarca("LG");
+        tele1.setModelo("A321");
+        tele1.setVolumen(90);
+        tele1.setCanal(4);
+        tele1.setBrillo(33);
+        tele1.setContraste(2);
+        System.out.println(tele1.getCanal());
+        System.out.println(tele1.getMarca());
+        System.out.println(Television.precio);
+        System.out.println("*************");
+
+        Television tele2 = new Television("Sony", "GN433");
+        System.out.println("Marca tele2: " +tele2.getMarca());
+        System.out.println("Modelo tele2: " +tele2.getModelo());
+        System.out.println("*************");
+
+        Television tele3 = new Television("Samsumg", "GN99", 40, 43, 3, 2);
+        System.out.println("Marca tele3: " +tele3.getMarca());
+        System.out.println("Modelo tele3: " +tele3.getModelo());
+        System.out.println("Volumen tele3: " +tele3.getVolumen());
+        System.out.println("Brillo tele3: " +tele3.getBrillo());
+        System.out.println("Contraste tele3: " +tele3.getContraste());
+        System.out.println("Canal tele3: " +tele3.getCanal());
+        System.out.println("*************");
+
+        // Formas para imprimir de forma masiva. Con un método o con .toString
+        System.out.println(tele3);
+        System.out.println("*************");
+
+        // Aplicando GETTERS y SETTERS
+        System.out.println(tele2.getMarca());
+        System.out.println(tele3.getMarca());
+        tele2.setMarca("Mitshubichi");
+        tele3.setMarca("Nokia");
+        System.out.println(tele2.getMarca());
+        System.out.println(tele3.getMarca());
+        System.out.println("*************");
+
+        // Public y Private
+        // Por seguridad, se establecen todas las clases private para evitar que los datos queden públicos y visibles
+        // a tercero. Para ello, se invocan o cambian con los getters y setters creados previamente.
+        tele1.imprimirReferencia();
+        System.out.println(tele1);
+        tele2.imprimirReferencia();
+        System.out.println(tele2);
+        tele3.imprimirReferencia();
+        System.out.println("*************");
+    }
 }
